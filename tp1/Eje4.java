c4c0b9867a163e82de751af4bd5dc3b7d935b372
MoyanoAgustinPiya <elarpondorado100@gmail.com>
2025-08-28 21:50:57 +0000

    modificaciones

--- tp1/Eje4.java
+++ tp1/Eje4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-  public class eje4.java 
+  public class tp1.Eje4
       public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese 3 números enteros");
@@ -8,15 +8,17 @@ import java.util.Scanner;
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
         if(num1 > num2 & num1 >num3) {
-            System.out.println("El mayor numero es: ", num1);}
-            else{
+            System.out.println("El mayor numero es: "+ num1);}
+            else
                 if(num2 > num1 & num2 > num3){
-                    System.out.println("El mayor numero es: ", num2);
-                else 
-                (System.out.println("El mayor numero es: ", num3);)
+                    System.out.println("El mayor numero es: "+ num2);
+                else {
+                    (System.out.println("El mayor numero es: "+ num3);
+                }
+                
                             
-}
-            }
+            
+            
         
-      }
+      
       scanner.close();
---
+++ tp2/TestPersona.java
@@ -0,0 +1,10 @@
+package tp2 
+public Class testPersona();
+public static void  main (Stringargs[]){
+int numero;
+Persona = new Persona ("Franco", "Héctor", "42.1234");
+Persona = new Persona ("Felipe", "Costa", "38.1255");
+    System.out.println(Persona.mayorDeEdad());
+    boolean esMayor;
+    esMayor=Persona.mayorDeEdad();
+}
